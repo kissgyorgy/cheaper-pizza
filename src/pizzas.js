2d7fd7c54c8dc9942b11c5cba6bf51c2d110ed97
Kiss György <kissgyorgy@me.com>
2020-09-27 14:38:37 +0200

    import images instead of using strings

--- src/pizzas.js
+++ src/pizzas.js
@@ -1,8 +1,13 @@
+import mixedPizzaImg from "./images/mixed.png";
+import olivePizzaImg from "./images/olive-salad.png";
+import pepperoniPizzaImg from "./images/pepperoni.png";
+import veggiesPizzaImg from "./images/veggies.png";
+
 const PIZZAS = [
-  { name: "Mixed", image: "images/mixed.png", diameter: 23, price: 1500, pieces: 3 },
-  { name: "Veggies", image: "images/veggies.png", diameter: 32, price: 1800, pieces: 2 },
-  { name: "Olives", image: "images/olive-salad.png", diameter: 45, price: 4000, pieces: 1 },
-  { name: "Pepperoni", image: "images/pepperoni.png", diameter: 50, price: 6000, pieces: 1 },
+  { name: "Mixed", image: mixedPizzaImg, diameter: 23, price: 1500, pieces: 3 },
+  { name: "Veggies", image: veggiesPizzaImg, diameter: 32, price: 1800, pieces: 2 },
+  { name: "Olives", image: olivePizzaImg, diameter: 45, price: 4000, pieces: 1 },
+  { name: "Pepperoni", image: pepperoniPizzaImg, diameter: 50, price: 6000, pieces: 1 },
 ];
 
 function getNextPizza(inputs) {
